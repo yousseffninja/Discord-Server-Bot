e6945634692be60c73febe1c63baed5c87e22e1a
backm <yousseffmohamedd22@gmail.com>
2023-06-25 19:14:53 +0300

    Fix some error in exporting

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv/config');
-
 const { Client, Intents } = require('discord.js');
 const { Configuration, OpenAIApi } = require('openai');
 
@@ -22,40 +21,13 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
-    if (message.channel.id !== process.env.CHANNEL_ID) return;
-    if (message.content.startsWith('!')) return;
+    // Rest of your message handling code
 
-    let conversationLog = [{ role: 'system', content: "you are a friendly chat bot" }];
-
-    conversationLog.push({
-        role: 'user',
-        content: message.content,
-    });
-
-    await message.channel.sendTyping();
-
-    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
-    prevMessages = prevMessages.array().reverse();
-
-    prevMessages.forEach((msg) => {
-        if (msg.content.startsWith('!')) return;
-        if (msg.author.id !== client.user.id && msg.author.bot) return;
-        if (msg.author.id !== message.author.id) return;
-        conversationLog.push({
-            role: 'user',
-            content: msg.content,
-        });
-    });
-
-    const result = await openAI.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: conversationLog,
-    });
-
-    message.reply(result.data.choices[0].message.content);
+    // ...
 });
 
-client.login(process.env.TOKEN);
-
+// Export the client after attaching event listeners
 module.exports = client;
+
+// Start the bot by logging in
+client.login(process.env.TOKEN);
