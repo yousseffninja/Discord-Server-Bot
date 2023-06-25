ed1bd2caf4d74af205290a297f39abe7b4f3ecb1
backm <yousseffmohamedd22@gmail.com>
2023-06-25 19:12:37 +0300

    Fix some error in exporting

--- index.js
+++ index.js
@@ -1,58 +1,61 @@
 require('dotenv/config');
 
-const { Client, IntentsBitField } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 const { Configuration, OpenAIApi } = require('openai');
 
 const client = new Client({
-
     intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ]
-})
+        Intents.FLAGS.Guilds,
+        Intents.FLAGS.GuildMessages,
+        Intents.FLAGS.MessageContent,
+    ],
+});
 
-const confuguration = new Configuration({
+const configuration = new Configuration({
     apiKey: process.env.API_KEY,
 });
 
-const openAI = new OpenAIApi(confuguration);
+const openAI = new OpenAIApi(configuration);
 
-client.on('ready', ()=> {
+client.on('ready', () => {
     console.log('Bot is ready');
 });
 
 client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
     if (message.channel.id !== process.env.CHANNEL_ID) return;
-    if(message.content.startsWith('!')) return;
+    if (message.content.startsWith('!')) return;
 
     let conversationLog = [{ role: 'system', content: "you are a friendly chat bot" }];
 
     conversationLog.push({
         role: 'user',
         content: message.content,
-    })
+    });
 
     await message.channel.sendTyping();
 
-    let prevMessage = await message.channel.messages.fetch({ limit: 15 });
-    prevMessage.reverse();
+    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+    prevMessages = prevMessages.array().reverse();
 
-    prevMessage.forEach((msg) => {
-        if (message.content.startsWith('!')) return;
-        if (msg.author.id !== client.user.id && message.author.bot) return;
+    prevMessages.forEach((msg) => {
+        if (msg.content.startsWith('!')) return;
+        if (msg.author.id !== client.user.id && msg.author.bot) return;
         if (msg.author.id !== message.author.id) return;
+        conversationLog.push({
+            role: 'user',
+            content: msg.content,
+        });
     });
 
-    const result  = await openAI.createChatCompletion({
+    const result = await openAI.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: conversationLog,
-    })
+    });
 
-    message.reply(result.data.choices[0].message);
+    message.reply(result.data.choices[0].message.content);
 });
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
 
 module.exports = client;
