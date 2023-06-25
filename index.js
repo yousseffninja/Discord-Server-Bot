31625c38c578e2b72a1d09816d404aa8231b1b1b
backm <yousseffmohamedd22@gmail.com>
2023-06-25 19:16:57 +0300

    Fix some error in exporting

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const { Configuration, OpenAIApi } = require('openai');
 
 const client = new Client({
     intents: [
-        Intents.FLAGS.Guilds,
-        Intents.FLAGS.GuildMessages,
-        Intents.FLAGS.MessageContent,
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.MESSAGE_CONTENTS,
     ],
 });
 
@@ -21,13 +21,40 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', async (message) => {
-    // Rest of your message handling code
+    if (message.author.bot) return;
+    if (message.channel.id !== process.env.CHANNEL_ID) return;
+    if (message.content.startsWith('!')) return;
 
-    // ...
+    let conversationLog = [{ role: 'system', content: "you are a friendly chat bot" }];
+
+    conversationLog.push({
+        role: 'user',
+        content: message.content,
+    });
+
+    await message.channel.sendTyping();
+
+    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+    prevMessages = prevMessages.array().reverse();
+
+    prevMessages.forEach((msg) => {
+        if (msg.content.startsWith('!')) return;
+        if (msg.author.id !== client.user.id && msg.author.bot) return;
+        if (msg.author.id !== message.author.id) return;
+        conversationLog.push({
+            role: 'user',
+            content: msg.content,
+        });
+    });
+
+    const result = await openAI.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: conversationLog,
+    });
+
+    message.reply(result.data.choices[0].message.content);
 });
 
-// Export the client after attaching event listeners
-module.exports = client;
-
-// Start the bot by logging in
 client.login(process.env.TOKEN);
+
+module.exports = client;
